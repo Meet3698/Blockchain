c6e531c971ac2ce563818215072a91ff5f2906b4
BabyVanila <bhavyabrijeshpatel@gmail.com>
2021-11-16 14:48:07 +0530

    'combine'

--- p2p/p2p.py
+++ p2p/p2p.py
@@ -9,8 +9,7 @@ class p2p:
     db = client['p2p']
     collection = db['nodes']
     
-    import socket
-    def get_ip_address(self):
+    def get_ip_address():
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
         return s.getsockname()[0]
