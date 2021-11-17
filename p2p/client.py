8e90571eb4c9c4d1989b715497757ea176d5ac42
BabyVanila <bhavyabrijeshpatel@gmail.com>
2021-11-18 00:20:40 +0530

    'commit'

--- p2p/client.py
+++ p2p/client.py
@@ -45,7 +45,8 @@ class Client:
 	def recieve_message(self):
 		try:
 			data = self.s.recv(byte_size)
-			print('Recieved data : {}'.format(data))
+			print('Recieved data : {}'.format(json.dumps({'data' : data})))
+
 			return data
 		except KeyboardInterrupt:
 			print('Disconnected From the server')
@@ -55,9 +56,8 @@ class Client:
 	def update_peers(self,peers):
 		print('In update peers --- ', str(peers,'utf-8').split(',')[:-1])
 		p2p.peers.append(str(peers,'utf-8').split(',')[:-1])
-		print('Updated peers --- ', p2p.peers) 
-
-		for peer in p2p.peers:
-			url = 'http://' + str(peer) + ':5000/connect_node'
-			r = requests.post(url, data = json.dumps({'peer' : p2p.peers}))
-			print(r)
+		print('Updated peers --- ', p2p.peers)
+		sleep(2)
+		url = 'http://' + str(host) + ':5000/connect_node'
+		r = requests.post(url, data = json.dumps({'peer' : p2p.peers}))
+		print(r)
