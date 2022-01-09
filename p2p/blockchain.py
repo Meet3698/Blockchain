14e0fbc5c8ab84e4cbeb45f5a218475f64485284
BabyVanila <bhavyabrijeshpatel@gmail.com>
2022-01-09 11:39:44 +0530

    'commit'

--- p2p/blockchain.py
+++ p2p/blockchain.py
@@ -98,6 +98,7 @@ class Blockchain:
         longest_chain = None
         max_length = len(self.chain)
         h = str(host) + ':5000'
+        print(network)
         for nodes in network:
             if nodes != h:
                 response = requests.get(f'http://{nodes}/get_chain')
