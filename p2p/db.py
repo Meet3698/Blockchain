fbf420da5e64025161f73f695042941594fbad16
Meet <49397344+Meet3698@users.noreply.github.com>
2022-02-03 14:39:37 +0530

    Update db.py

--- p2p/db.py
+++ p2p/db.py
@@ -2,7 +2,7 @@ import pymongo
 
 class DB:
     def __init__(self):
-        self.client = pymongo.MongoClient("mongodb+srv://Jack:Jack5972@blockchain.2n0ho.mongodb.net/p2p?retryWrites=true&w=majority")
+        self.client = pymongo.MongoClient("<API>")
         self.db = self.client['p2p']
         self.collection_nodes = self.db['nodes']
         self.collection_voter_details = self.db['voter_details']
